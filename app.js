ba0aaba3c6c177203dfb6301f297d2c5ed641f29
João <58960937+JoaoMesquitaL@users.noreply.github.com>
2024-01-22 21:24:24 -0300

    Teste de condições de chamadas no bot
    implementação de um simples menu e resposta padrão para tratar mensagens que não foram configuradas ao bot.

--- app.js
+++ app.js
@@ -17,12 +17,52 @@ function start(client){
     client.onMessage((message) => {
         if(message.body === "Hi" ) {
             client
-                .sendText(message.from, "Sucesso")
+                .sendText(message.from, "Olá sou seu Bot the Whatsapp, o que deseja fazer? \n\n 1-Qual seu objetivo? \n 2-Como entro em contato? \n 3-Qual sua origem?")
                 .then((result) => {
                     console.log("Result: ", result);
                 })
                 .catch((erro) => {
-                    console.error("Error whem sending: ", erro);
+                    console.error("Não foi possível responder o user: ", erro);
+                })
+        }
+        else if(message.body === '1'){
+            client
+                .sendText(message.from, "No momento estou em fase de desenvolvimento, mas fui idealizado pensando em como ajudar diversos tipos de atendimentos ao cliente!")
+                .then((result) =>{
+                    console.log("Result: ", result);
+                })
+                .catch((erro) => {
+                    console.error("Não foi possível responder o user: ", erro);
+                })
+        }
+        else if(message.body === '2'){
+            client
+                .sendText(message.from, "Podemos continuar conversando por aqui, ou caso você deseje um contato pode ir no Github/JoaoMesquitaL ou Github/ThiagoReisFreitas")
+                .then((result) =>{
+                    console.log("Result: ", result);
+                })
+                .catch((erro) => {
+                    console.error("Não foi possível responder o user: ", erro);
+                })
+        }
+        else if(message.body === '3'){
+            client
+                .sendText(message.from, "Sou originado da biblioteca Venom Bot que permite que você inicie o desenvolvimento de um bot de Whatsapp atraves de tecnologia como NodeJS e Javascript")
+                .then((result) =>{
+                    console.log("Result: ", result);
+                })
+                .catch((erro) => {
+                    console.error("Não foi possível responder o user: ", erro);
+                })
+        }
+        else{
+            client
+                .sendText(message.from, "Desculpe, acho que não te entendi")
+                .then((result) =>{
+                    console.log("Result: ", result);
+                })
+                .catch((erro) => {
+                    console.error("Interação invalida do usuario: ", erro);
                 })
         }
     })
